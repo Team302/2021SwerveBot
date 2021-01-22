45579d8e845b250438d5f2d9d0f07e3d00b5f2a1
AJ <LOGdowskiA56@stu.lok12.org>
2021-01-21 19:55:16 -0500

    Starting the progress on auton drive forward

---
+++ src/main/cpp/auton/primitives/driveForawrd.cpp
@@ -0,0 +1,15 @@
+#include <auton/PrimitiveParams.h>
+#include<auton/primitives/IPrimitive.h>
+#include <auton/primitives/driveForward.h>
+#include <subsys/SwerveChassisFactory.h>
+#include <subsys/SwerveChassis.h>
+
+using namespace std;
+using namespace frc;
+
+driveForward::driveForward() :
+m_minimum_time(0),
+
+{
+    
+}
